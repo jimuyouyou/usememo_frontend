22fa993799aa64d2c97166edc6e9394c307c4f1a
jimuyouyou <qinfuxian2014@outlook.com>
2024-01-17 16:59:16 +0800

    http-only frontend-nodejs-apollo client works

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,21 +1,62 @@
-'use client';
-
-import { authenticate } from '@/app/lib/actions';
-import { lusitana } from '@/app/ui/fonts';
+"use client";
+import { useState } from "react";
+import { authenticate } from "@/app/lib/actions";
+import { lusitana } from "@/app/ui/fonts";
 import {
   AtSymbolIcon,
   KeyIcon,
   ExclamationCircleIcon,
-} from '@heroicons/react/24/outline';
-import { ArrowRightIcon } from '@heroicons/react/20/solid';
-import { Button } from './button';
-import { useFormState, useFormStatus } from 'react-dom';
+} from "@heroicons/react/24/outline";
+import { ArrowRightIcon } from "@heroicons/react/20/solid";
+import { useFormState, useFormStatus } from "react-dom";
+import { useRouter } from "next/navigation";
+import { Button } from "./button";
+import { setAccessToken } from "../lib/apolloClient";
 
 export default function LoginForm() {
+  const [email, setEmail] = useState("lisa@simpson.com");
+  const [password, setPassword] = useState("secret42");
+  const [error, setError] = useState("");
   const [errorMessage, dispatch] = useFormState(authenticate, undefined);
+  const router = useRouter();
+
+  const handleLogin = async () => {
+    // Basic email and password validation
+    if (!email.trim() || !password.trim()) {
+      setError("Please enter both email and password.");
+      return;
+    }
+
+    // dispatch({
+    //   email: email.trim(),
+    //   password: password.trim(),
+    // });
+    const res = await fetch("api/user/login", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email.trim(),
+        password: password.trim(),
+      }),
+    });
+
+    // set jwt token into following gql headers
+    const data = await res.json();
+    if (data && data.accessToken) {
+      setAccessToken();
+      router.push("/dashboard");
+    }
+    console.log("data", data);
+
+    // Add your login logic here (e.g., API call, authentication)
+    console.log(`Logging in with email: ${email} and password: ${password}`);
+  };
 
   return (
-    <form action={dispatch} className="space-y-3">
+    <div className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
           Please log in to continue.
@@ -34,7 +75,8 @@ export default function LoginForm() {
                 id="email"
                 type="email"
                 name="email"
-                value="lisa@simpson.com"
+                value={email}
+                onChange={()=>{}}
                 placeholder="Enter your email address"
                 required
               />
@@ -54,7 +96,8 @@ export default function LoginForm() {
                 id="password"
                 type="password"
                 name="password"
-                value="secret42"
+                value={password}
+                onChange={()=>{}}
                 placeholder="Enter password"
                 required
                 minLength={6}
@@ -63,7 +106,8 @@ export default function LoginForm() {
             </div>
           </div>
         </div>
-        <LoginButton />
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        <LoginButton handleLogin={handleLogin} />
         <div
           className="flex h-8 items-end space-x-1"
           aria-live="polite"
@@ -77,16 +121,34 @@ export default function LoginForm() {
           )}
         </div>
       </div>
-    </form>
+    </div>
   );
 }
 
-function LoginButton() {
+interface MyComponentProps {
+  handleLogin: () => void; // Adjust the function signature based on your requirements
+}
+
+const LoginButton: React.FC<MyComponentProps> = ({ handleLogin }) => {
   const { pending } = useFormStatus();
 
   return (
-    <Button className="mt-4 w-full" aria-disabled={pending}>
+    <Button
+      className="mt-4 w-full"
+      aria-disabled={pending}
+      onClick={handleLogin}
+    >
       Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
     </Button>
   );
-}
+};
+
+// function LoginButton({handleLogin: MyComponentProps }) {
+//   const { pending } = useFormStatus();
+
+//   return (
+//     <Button className="mt-4 w-full" aria-disabled={pending} onClick={ handleLogin}>
+//       Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+//     </Button>
+//   );
+// }
